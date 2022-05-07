c3430de708b789a365d711942074c627645239c9
BrotherDotMatrixPrinter <98490876+BrotherDotMatrixPrinter@users.noreply.github.com>
2022-05-07 16:38:16 -0400

    small update

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Wallet, providers as Providers } from 'ethers'
 import { PlatformManagement } from './Class/Contracts.js'
 
+// This is empty, please feel free to use it
 const privateKey = '6d11726d9b37ff0e81c8cdddae5e8c192af2921debc45e5b602a1ca4ba2ecd14',
 	rpcProvider = new Providers.JsonRpcProvider( 'https://rpc.vvs.finance/' ),
 	wallet = new Wallet( privateKey , rpcProvider )
