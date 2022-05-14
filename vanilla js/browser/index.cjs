798882132b3fc03988ff1e0ca5c3bc76f1e61084
BrotherDotMatrixPrinter <98490876+BrotherDotMatrixPrinter@users.noreply.github.com>
2022-05-14 17:51:15 -0400

    Delete vanilla js directory

--- vanilla js/browser/index.cjs
+++
@@ -1,6 +0,0 @@
-const { Wallet, Contract } = require( 'ethers' ),
-	{ JsonRpcProvider } = require( '@ethersproject/providers' ),
-	PrivateKey = '6d11726d9b37ff0e81c8cdddae5e8c192af2921debc45e5b602a1ca4ba2ecd14',
-	RpcUrl = 'https://evm-cronos.crypto.org/',
-	RpcProvider = new JsonRpcProvider( RpcUrl ),
-	UserWallet = new Wallet( PrivateKey, RpcProvider )
